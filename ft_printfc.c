e31e776767fc24353c9999e36e1cff75053229b0
Yik Wai Chan <yichan@u81z08s03.42kl.edu.my>
2022-07-20 23:34:02 +0800

    add

--- ft_printfc.c
+++ ft_printfc.c
@@ -6,7 +6,7 @@
 /*   By: yichan <yichan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 23:04:07 by yichan            #+#    #+#             */
-/*   Updated: 2022/07/20 21:01:56 by yichan           ###   ########.fr       */
+/*   Updated: 2022/07/20 22:44:09 by yichan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_printfc(t_flag *fmt)
 	const char	c = ca_ard(fmt->args, char *);
 
 	if (fmt->width > fmt->precision)
-		fmt
+		
 	
 
 }
